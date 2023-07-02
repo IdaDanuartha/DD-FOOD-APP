cbf766c16675b5b60cf0d5c7178d8d9f85dd347f
IdaDanuartha <danuart21@gmail.com>
2023-07-02 17:57:14 +0800

    styling orders page (90%)

--- resources/views/livewire/customer/orders.blade.php
+++ resources/views/livewire/customer/orders.blade.php
@@ -1,3 +1,56 @@
 <div>
-    <h2>Orders page</h2>
+    <h1 class="text-xl font-bold">Orders</h1>
+    <div class="my-5 p-6 bg-white rounded-lg">
+        <h3 class="mb-4 font-semibold">My Orders</h3>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-white uppercase bg-main-color dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            No Table
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            My Queue
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Current Queue
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Status
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Total
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td class="px-6 py-4 text-gray-800">
+                            14
+                        </td>
+                        <td class="px-6 py-4 text-gray-800">
+                            #104
+                        </td>
+                        <td class="px-6 py-4 text-gray-800">
+                            #98
+                        </td>
+                        <td class="px-6 py-4 text-gray-800">
+                            Waiting
+                        </td>
+                        <td class="px-6 py-4 text-gray-800">
+                            Rp. 79.500
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-gray-800">
+                                <i class="fa-solid fa-file-lines fa-xl"></i>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>

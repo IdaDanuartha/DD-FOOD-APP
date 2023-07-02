2e35621ecd9a0a3b74fac2b301f94a8060b7e216
IdaDanuartha <danuart21@gmail.com>
2023-07-02 15:25:46 +0800

    add modal menu

--- resources/views/components/menu-card.blade.php
+++ resources/views/components/menu-card.blade.php
@@ -1,20 +1,52 @@
-@foreach ($menus as $menu)
+@foreach ($menus as $index => $menu)
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-            <img class="rounded-t-lg" src="https://picsum.photos/400/200" alt="" />
-        </a>
+        <div data-modal-target="modal-menu-{{ $index }}" data-modal-toggle="modal-menu-{{ $index }}">
+            <img class="rounded-t-lg cursor-pointer" src="https://picsum.photos/400/200" alt="" />
+        </div>
         <div class="px-5 py-3">
             <a href="#">
                 <h5 class="text-sm mb-1 tracking-tight text-main-color dark:text-white">Junkfood</h5>
             </a>
-            <a href="#">
-                <h5 class="mb-2 text-md font-semibold tracking-tight text-gray-900 dark:text-white">{{ $menu['name'] }}</h5>
-            </a>
+            <div data-modal-target="modal-menu-{{ $index }}" data-modal-toggle="modal-menu-{{ $index }}">
+                <h5 class="cursor-pointer mb-2 text-md font-semibold tracking-tight text-gray-900 dark:text-white">{{ $menu['name'] }}</h5>
+            </div>
             <div class="flex justify-between items-center">
-                <p class="mb-3 text-sm text-gray-700 dark:text-gray-400">Rp. 25.000</p>
-                <a href="#" class="px-5 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <p class="text-sm text-gray-700 dark:text-gray-400">Rp. 25.000</p>
+                <a href="#" class="px-5 py-2 text-xs font-medium text-center text-white bg-main-color rounded-lg hover:bg-main-color/90 focus:ring-4 focus:outline-none focus:ring-main-color/50 dark:bg-main-color dark:hover:bg-main-color dark:focus:ring-main-color">
                 Add
                 </a>
+            </div>
+        </div>
+    </div>
+
+    <div id="modal-menu-{{ $index }}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative w-full max-w-2xl max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        Terms of Service
+                    </h3>
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="modal-menu-{{ $index }}">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-6 space-y-6">
+                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                        With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                    </p>
+                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                        The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                    </p>
+                </div>
+                <!-- Modal footer -->
+                <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                    <button data-modal-hide="modal-menu-{{ $index }}" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                    <button data-modal-hide="modal-menu-{{ $index }}" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+                </div>
             </div>
         </div>
     </div>
